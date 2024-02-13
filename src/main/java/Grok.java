e9c71f6ce83c4945f1b712b4b172eaaaeec564c4
jcampos3 <johana.m.c.3005@gmail.com>
2024-02-13 17:42:14 -0500

    finished

--- src/main/java/Grok.java
+++ src/main/java/Grok.java
@@ -9,5 +9,28 @@
  */
 public class Grok
 {
-    // implement this class per instructions in Required Activities.
+    // implement this class per instructions in Required Activities
+    private static final int DEFAULT_POWER_LEVEL = 50;
+    private int powerLevel;
+    public Grok(){
+        powerLevel = DEFAULT_POWER_LEVEL;
+    }
+    public Grok(int powerLevel){
+        this.powerLevel = powerLevel;
+    }
+
+    public int getPowerLevel() {
+        return powerLevel;
+    }
+
+    public void setPowerLevel(int powerLevel) {
+        this.powerLevel = powerLevel;
+    }
+    public void takePowerPill(PowerPill nugget){
+        setPowerLevel(getPowerLevel() + nugget.getPower());
+    }
+    public void tookHit(){
+        powerLevel -= 5;
+    }
+
 }
